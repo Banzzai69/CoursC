266201683da8008d8be918d01ef0c8ad036bb47f
Banzzai69 <150901470+Banzzai69@users.noreply.github.com>
2024-01-09 15:31:19 +0000

    v.0.0.0.3: ajout fonction char et float

--- main.c
+++ main.c
@@ -5,8 +5,11 @@ int main()
     int num = 20;
     int num2 = 41;
     float price =  15.99; 
+    char c ='c';
+
     printf("Hello world %d \n", num2);
-    printf("la valeur de num = %d, la valeur de num2 = %i, prix =%.2f \n", num , num2 , price) ;
+    
+    printf("la valeur de num = %d, la valeur de num2 = %i, prix =%.2f \n, char value: %c \n", num , num2 , price, c) ;
     
     return 0;
 

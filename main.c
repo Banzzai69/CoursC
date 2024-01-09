a9c694d41f623077a31a47fe00db8a5447740bd3
Banzzai69 <150901470+Banzzai69@users.noreply.github.com>
2024-01-09 13:30:51 +0000

    creation main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int nombre;
+    printf("Entrez un nombre : ");
+    scanf("%d", &nombre);
+    printf("Vous avez entré : %d\n", nombre);
+
+    return 0;
+
+}

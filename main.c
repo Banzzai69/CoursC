dd8421e4ac23670381e9415cf96f71ccccdf3b01
Banzzai69 <150901470+Banzzai69@users.noreply.github.com>
2024-01-09 14:30:47 +0000

    :v 0.0.2 : essai d'instruction

--- main.c
+++ main.c
@@ -4,8 +4,9 @@ int main()
 {
     int num = 20;
     int num2 = 41;
+    float price =  15.99; 
     printf("Hello world %d \n", num2);
-    printf("%d\n", num);
+    printf("la valeur de num = %d, la valeur de num2 = %i, prix =%.2f \n", num , num2 , price) ;
     
     return 0;
 
